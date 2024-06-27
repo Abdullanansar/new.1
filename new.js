09436aab3b446feecd29699043df757fa9f1045b
practice.1 <abdullahansar201@gmail.com>
2024-06-27 13:36:02 -0700

    deploying

--- new.js
+++ new.js
@@ -1,46 +1,151 @@
-// function reverseString(str) {
-//     return str.split('').reverse().join('');
-//   }
+// // // function reverseString(str) {
+// // //     return str.split('').reverse().join('');
+// // //   }
   
-//   const string = 'hello world';
-//   const reversedString = reverseString(string);
-//   console.log(reversedString); 
+// // //   const string = 'hello world';
+// // //   const reversedString = reverseString(string);
+// // //   console.log(reversedString); 
 
 
 
-function reverseString(str) {
-    return str.split("").reverse().join("");
-  }
+// // function reverseString(str) {
+// //     return str.split("").reverse().join("");
+// //   }
   
-  const string = "hello world";
-  const reversedString = reverseString(string);
-  console.log(reversedString);
+// //   const string = "hello world";
+// //   const reversedString = reverseString(string);
+// //   console.log(reversedString);
 
 
 
 
 
-  function redirectToUrl(url) {
-    window.location.href = url;
-  }
+// //   function redirectToUrl(url) {
+// //     window.location.href = url;
+// //   }
 
 
 
 
-  const fs = require('fs');
+// //   const fs = require('fs');
 
-function getLinesFromFile(filePath) {
-  try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    return data.split('\n');
-  } catch (err) {
-    console.error(`Error reading file: ${err}`);
-    return [];
-  }
-}
-
-
+// // function getLinesFromFile(filePath) {
+// //   try {
+// //     const data = fs.readFileSync(filePath, 'utf8');
+// //     return data.split('\n');
+// //   } catch (err) {
+// //     console.error(`Error reading file: ${err}`);
+// //     return [];
+// //   }
+// // }
 
 
 
 
+
+
+
+
+
+// let greet = (name) => {
+//   return 'Hello '+name;
+// }
+// console.log(greet("Abdullah"));
+
+
+
+
+
+
+// const sqr = num => num*num;
+// console.log(sqr(5));
+
+
+
+
+
+
+// var name = "Abdullah";
+// console.log(`Welcome to ${name} ! Have a nice day`);
+
+
+
+
+
+// if (condition) {
+//   var x = "hello";
+// }
+// document.write(x)
+
+
+
+
+// let user = "Abdullah";
+// let marks = 489;
+// document.write(`Hello ${user} your marks is ${marks}`);
+
+
+
+// let hello = () => console.log("Hello");
+
+
+
+
+
+
+// //           ALERTS
+
+// // function abdullah() {
+// //     alert("hello")
+// // }
+
+// //        VARIABLE FOR STRINGS   AND NUMBERS
+
+// // var a = "Abdullah ";
+// // var b =   12;
+// // document.write(a+b)
+
+// //         IF ELSE AND ELSE IF STATEMENT            //
+
+// // var a = prompt("write a code");
+// // if (a === "abdullah") {
+// //     document.getElementById("link").innerHTML
+// // }
+// // else{
+// //     alert("no")
+// // }
+
+
+
+
+// //            PRACTICE OF ARRAYS             //
+
+// // var a = [1,2,3,4,5,6,7,8,9]
+// // console.log(a.unshift(0));
+
+// // const x = ['abdullah','nehal','hassan','nafay']
+// // console.log(x.lastIndexOf(x.includes()));
+
+
+// // var found = x.find(Abdullah > ansar)
+// // console.log(found);
+
+
+
+
+// // var x = [1,2,3,4,5]
+
+// // const found = x.find((element) => element > 3);
+
+// // console.log(found);
+// // Expected output: 12
+
+// // function abdullah(a,b) {
+// //     return a+b;
+
+// // }
+// // function getUserInput() {
+// //     let userInput = document.getElementById("userInputField").value;
+// //     console.log("User input:", userInput);
+// //     // Further processing of userInput
+// //   }
